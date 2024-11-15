e8c6efd9b32a286c42f040dbf4dd2d5bc65f42e0
Mauricio Castillo <63652645+m4ujo@users.noreply.github.com>
2024-11-15 05:32:11 -0500

    test public path content

--- src/app/(pages)/preguntas/[slug]/page.tsx
+++ src/app/(pages)/preguntas/[slug]/page.tsx
@@ -4,7 +4,8 @@ import MarkdownContent from "@/components/markdown-content";
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const file = (await params).slug;
-  const filePath = path.join(process.cwd(), "src", "assets", "content", `${file}.md`);
+  // const filePath = path.join(process.cwd(), "src", "assets", "content", `${file}.md`);
+  const filePath = path.join(process.cwd(), "public", "content", `${file}.md`);
   const content = await fs.readFile(filePath, "utf-8");
 
   return <MarkdownContent content={content} />;
