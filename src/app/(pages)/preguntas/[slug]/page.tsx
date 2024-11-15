690c10aae3523876febf0e88704272a05da570a9
Mauricio Castillo <63652645+m4ujo@users.noreply.github.com>
2024-11-15 05:37:51 -0500

    update filepath in formulas/slug/page

--- src/app/(pages)/preguntas/[slug]/page.tsx
+++ src/app/(pages)/preguntas/[slug]/page.tsx
@@ -1,12 +1,5 @@
-import { promises as fs } from "fs";
-import path from "path";
-import MarkdownContent from "@/components/markdown-content";
-
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const file = (await params).slug;
-  // const filePath = path.join(process.cwd(), "src", "assets", "content", `${file}.md`);
-  const filePath = path.join(process.cwd(), "public", "content", `${file}.md`);
-  const content = await fs.readFile(filePath, "utf-8");
 
-  return <MarkdownContent content={content} />;
+  return <div>Test</div>;
 }
